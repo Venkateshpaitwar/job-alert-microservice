70b2d6216594bf6397552bda27417b7b8ab8ed7b
Venkateshpaitwar <163770071+Venkateshpaitwar@users.noreply.github.com>
2026-06-02 18:26:15 +0530

    Feat : Initial project setup

---
+++ job-alert-service/src/main/java/com/jobalert/job_alert_service/JobAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jobalert.job_alert_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAlertServiceApplication.class, args);
+	}
+
+}
---
+++ job-alert-service/src/test/java/com/jobalert/job_alert_service/JobAlertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobalert.job_alert_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAlertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
